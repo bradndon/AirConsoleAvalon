06a0edb907bd578b8f3538a7b5eeb6a037d879d1
Brandon Fox <bl.fox@live.com>
2016-12-13 22:20:17 -0800

    Update playerChooser to use redux

--- src/containers/PlayerChooser/PlayerChooserContainer.js
+++ src/containers/PlayerChooser/PlayerChooserContainer.js
@@ -3,52 +3,35 @@ import {PlayerCard, LeaderQuestInfo} from 'components'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as playerActionCreators from 'redux/modules/players'
-
+import * as voteActionCreators from 'redux/modules/votes'
 
 class PlayerChooserContainer extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      selectedCount: 0,
-      maxSelected: 4,
-    }
-  }
-  componentDidMount() {
-    console.warn(this.props)
+  componentWillMount() {
+    // TODO: move to before this component opens
+    this.props.addVote({}, 1)
   }
   handleTap(index, e) {
     e.stopPropagation()
-    this.props.choosePlayer(this.props.players[index].deviceId)
-
-    // let selectedCount = this.state.selectedCount
-    // if (players[index].chosen) {
-    //   if (selectedCount < this.state.maxSelected) {
-    //     selectedCount++;
-    //   } else {
-    //     players[index].chosen = !players[index].chosen
-    //   }
-    // } else {
-    //   selectedCount--;
-    // }
-    // this.setState({players, selectedCount})
+    this.props.voteOrUnvotePlayer(this.props.players[index].deviceId)
   }
   questReady () {
     console.warn('Ready for quest')
     this.context.router.push('/vote')
   }
   render () {
-    let numLeft = this.state.maxSelected - this.state.selectedCount
+    var currentCount = Object.keys(this.props.vote.players)
+      .reduce((a,b)=> this.props.vote.players[b] === true ? a+1 : a, 0)
     return (
       <div>
         <LeaderQuestInfo
-          numLeft={numLeft}
-          isReady={numLeft !== 0}
+          numLeft={this.props.vote.playerLimit - currentCount}
+          isReady={this.props.vote.playerLimit - currentCount !== 0}
           sendReady={()=>this.questReady()}/>
 
         {this.props.players.map((player, i)=><PlayerCard
           key={i}
           name={player.name}
-          chosen={player.chosen}
+          chosen={this.props.vote.players[player.deviceId] === true}
           handleTap={(e) =>this.handleTap(i, e)}/>)}
         <div style={{clear: 'both', paddingTop: '90px'}}/>
       </div>
@@ -60,14 +43,18 @@ PlayerChooserContainer.contextTypes = {
   router: PropTypes.object.isRequired,
 }
 
-function mapStateToProps({players}) {
+function mapStateToProps({players, votes}) {
   return {
     players: Object.keys(players).map((e)=>typeof players[e] === 'object'? players[e]:null).filter((e)=>e),
+    vote: votes[votes.length-1],
   }
 }
 
 function mapDispatchToProps (dispatch) {
-  return bindActionCreators(playerActionCreators, dispatch)
+  return bindActionCreators({
+    ...playerActionCreators,
+    ...voteActionCreators,
+  }, dispatch)
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(PlayerChooserContainer)
