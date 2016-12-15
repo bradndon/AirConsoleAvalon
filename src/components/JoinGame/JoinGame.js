012cb5626db9712b17f0e7dceacf24c9d4117f3a
Brandon Fox <bl.fox@live.com>
2016-12-14 16:48:50 -0800

    Add styled-components and convert JoinGame

--- src/components/JoinGame/JoinGame.js
+++ src/components/JoinGame/JoinGame.js
@@ -1,12 +1,50 @@
 import React, { PropTypes } from 'react'
 import './JoinGame.css'
+import styled from 'styled-components'
+
+const Form = styled.form`
+  background-color: rgba(0,0,50,0.6);
+  padding: 50px;
+  width: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
+const TextInput = styled.input`
+  font-size: 24px;
+  padding: 10px;
+  margin-bottom: 30px;
+  color: white;
+  background-color: transparent;
+  border: none;
+  width: 70%;
+  text-align: center;
+  border-bottom: 3px solid white;
+  transition: all .3s;
+
+  &:focus {
+    border-bottom: 3px solid #606060;
+    outline: none;
+  }
+`
+
+const SubmitButton = styled.input`
+  background-color: white;
+  border: none;
+  border-radius: 25px;
+  font-size: 16px;
+  padding: 10px 15px;
+`
 
 const JoinGame = ({handleSubmit, handleChange, name}) => {
   return (
-      <form  className={'joinGame'} onSubmit={handleSubmit}>
-        <input type='text' value={name} onChange={handleChange}/>
-        <input type='submit' value='Join Game'/>
-      </form>
+      <Form onSubmit={handleSubmit}>
+        <TextInput type='text' value={name} onChange={handleChange}/>
+        <SubmitButton type='submit' value='Join Game'/>
+      </Form>
   )
 }
 
