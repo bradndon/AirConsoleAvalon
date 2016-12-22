0cfa8b9b9f8a39b949ca9de332fdacb48902b8d1
Brandon Fox <bl.fox@live.com>
2016-12-22 12:59:24 -0800

    Add quests reducer tests

---
+++ screen/src/spec/redux/modules/quests.test.js
@@ -0,0 +1,55 @@
+import * as actions from 'redux/modules/quests'
+import reducer from 'redux/modules/quests'
+import * as types from 'constants/ActionTypes'
+
+describe("Quests", ()=> {
+  describe("Reducer", ()=> {
+    it("should return the inital state", ()=> {
+      expect(reducer(undefined, {})).toEqual({
+        1: {
+          votes: {}
+        },
+        2: {
+          votes: {}
+        },
+        3: {
+          votes: {}
+        },
+        4: {
+          votes: {}
+        },
+        5: {
+          votes: {}
+        }
+      })
+    })
+    it("should add a vote id to a quest", ()=> {
+      expect(reducer({
+        1: {
+          votes: {}
+        }
+      }, {
+        type: types.QUEST_ADD_VOTE,
+        questId: 1,
+        voteId: 1
+      })).toEqual({
+        1: {
+          votes: {1: true}
+        }
+      })
+    })
+    it("should set a vote to the official vote", () => {
+      expect(reducer({
+        1: {}
+      }, {
+        type: types.QUEST_SET_VOTE,
+        voteId: 1
+      })).toEqual({
+        1: {
+          finalVote: 1,
+        }
+      })
+    })
+
+  })
+})
