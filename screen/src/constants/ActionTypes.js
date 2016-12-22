698e60070e24483e20b687dd49c0050bc44a318f
Brandon Fox <bl.fox@live.com>
2016-12-22 14:49:59 -0800

    Add action types

--- screen/src/constants/ActionTypes.js
+++ screen/src/constants/ActionTypes.js
@@ -3,5 +3,9 @@ export const ADD_ROLE = 'ADD_ROLE'
 export const ADD_VOTE = 'ADD_VOTE'
 export const VOTE_PLAYER = 'VOTE_PLAYER'
 export const UNVOTE_PLAYER = 'UNVOTE_PLAYER'
+
+//Quest Actions
 export const QUEST_ADD_VOTE = 'QUEST_ADD_VOTE'
 export const QUEST_SET_VOTE = 'QUEST_SET_VOTE'
+export const QUEST_SUCCESS = 'QUEST_SUCCESS'
+export const QUEST_FAILURE = 'QUEST_FAILURE'
