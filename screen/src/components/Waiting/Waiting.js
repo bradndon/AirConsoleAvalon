fd79ad0dca7b46924d1fa54414ca2d58ef5ea29b
Brandon Fox <bl.fox@live.com>
2016-12-24 14:12:29 -0800

    Show Game Ready if proper amount of players

--- screen/src/components/Waiting/Waiting.js
+++ screen/src/components/Waiting/Waiting.js
@@ -1,11 +1,13 @@
 import React, { PropTypes } from 'react'
 import "./Waiting.css"
 
-const Waiting = (props) => {
+const Waiting = ({players}) => {
+  const GameReady = players.length >= 5 && players.length <= 10 ? <h3>Ready to start!</h3> : null
   return (
     <div className={"waiting"}>
       <h2>Players joined:</h2>
-      {props.players.map((player, index)=><p key={index}>{player}</p>)}
+      {players.map((player, index)=><p key={index}>{player}</p>)}
+      {GameReady}
     </div>
   )
 }
