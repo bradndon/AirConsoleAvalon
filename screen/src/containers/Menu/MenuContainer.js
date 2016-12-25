a9b7fea46cc522cc4b21dc082f4e06922839e257
Brandon Fox <bl.fox@live.com>
2016-12-24 21:23:06 -0800

    Add player names from store to container

--- screen/src/containers/Menu/MenuContainer.js
+++ screen/src/containers/Menu/MenuContainer.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { Waiting } from "components"
+import { connect } from 'react-redux'
+import { getPlayerNames } from 'redux/modules/players'
 
 class MenuContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {players: []};
-  }
   render () {
     return (
-      <Waiting players={this.state.players}/>
+      <Waiting players={this.props.players}/>
     )
   }
 }
 
-export default MenuContainer;
+function mapStateToProps(state, props) {
+  return {
+    players: getPlayerNames(state, props)
+  }
+}
+
+export default connect(mapStateToProps)(MenuContainer)
