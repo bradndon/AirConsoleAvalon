36b1f3ca1b4610e33ca6e22aa5757ad89c591de4
Brandon Fox <brandon@toolhouse.com>
2016-12-27 11:58:25 -0800

    Add test for airconsole, remove output from build

---
+++ screen/src/spec/constants/airconsole.test.js
@@ -0,0 +1,11 @@
+import AirConsole from 'airconsole-api/airconsole-1.6.0.js'
+import airconsole from 'constants/airconsole'
+import store from 'redux/store'
+import {addPlayer} from 'redux/modules/players'
+
+describe("airconsole", function() {
+  it("should dispatch the action it recieves onMessage", ()=> {
+    airconsole.onMessage(1, addPlayer("Brandon", 1))
+    expect(store.getState().players[1]).toEqual({"deviceId": 1, "name": "Brandon", "role": ""})
+  })
+});
