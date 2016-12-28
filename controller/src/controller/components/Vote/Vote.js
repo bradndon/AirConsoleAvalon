d884e85dc37378e1a6624bc6ed20fd6bd8f7b308
Brandon Fox <brandon@toolhouse.com>
2016-12-28 14:28:49 -0800

    Add logic to submit vote

--- controller/src/controller/components/Vote/Vote.js
+++ controller/src/controller/components/Vote/Vote.js
@@ -2,27 +2,27 @@ import React, { PropTypes } from 'react'
 import './Vote.css'
 import { Button } from 'controller/elements'
 
-const Vote = ({isApproving, approve, reject, names, lastName}) => {
+const Vote = ({isApproving, approve, reject, names, lastName, submitVote}) => {
   return (
     <div className={'vote'}>
       <div className={'vote__small'}>
-        <p>{`Should ${names.map((e)=>e + ', ')} and ${lastName} go on this quest?`}</p>
+        <p>{`Should ${names.join(', ')} go on this quest?`}</p>
       </div>
       <div onClick={approve} className={'vote__option vote__option--approve ' + (isApproving ? 'vote__option--selected' : '')}><p>{'Approve'}</p></div>
       <div onClick={reject} className={'vote__option vote__option--reject ' + (!isApproving ? 'vote__option--selected' : '')}><p>{'Reject'}</p></div>
       <div className={'vote__small'}>
-        <Button className={'vote__submit'} text={"Submit Vote"}/>
+        <Button onClick={submitVote} text={"Submit Vote"}/>
       </div>
     </div>
   )
 }
 
 Vote.propTypes = {
-  lastName: PropTypes.string.isRequired,
   names: PropTypes.array.isRequired,
   isApproving: PropTypes.bool.isRequired,
   approve: PropTypes.func.isRequired,
   reject: PropTypes.func.isRequired,
+  submitVote: PropTypes.func.isRequired,
 }
 
 export default Vote
