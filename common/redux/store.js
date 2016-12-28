ca0d533c40777ecedf3f8c46c4d790e561ef6f1a
Brandon Fox <brandon@toolhouse.com>
2016-12-28 11:56:31 -0800

    Add middleware to dispatch url changes

--- common/redux/store.js
+++ common/redux/store.js
@@ -1,6 +1,13 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import * as reducers from 'redux/modules'
-import { routerReducer } from 'react-router-redux'
+import { routerMiddleware, routerReducer } from 'react-router-redux'
+import { hashHistory } from 'react-router'
+
+const middleware = routerMiddleware(hashHistory)
+
 
 export default createStore(combineReducers({...reducers, routing: routerReducer}),
-  window.devToolsExtension ? window.devToolsExtension() : (f)=>f)
+  compose(
+    applyMiddleware(middleware),
+    window.devToolsExtension ? window.devToolsExtension() : (f)=>f,
+  ))
